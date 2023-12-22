31935224ca4f40b276a911b8fd3bd5242328c3a0
Szymon Ochnio <szymonochnio15@gmail.com>
2023-12-22 14:38:12 +0100

    Project structure by Spring Initializr.

---
+++ banking-be/src/main/java/pl/ochnios/bankingbe/BankingBeApplication.java
@@ -0,0 +1,13 @@
+package pl.ochnios.bankingbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingBeApplication.class, args);
+    }
+
+}
---
+++ banking-be/src/test/java/pl/ochnios/bankingbe/BankingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ochnios.bankingbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
