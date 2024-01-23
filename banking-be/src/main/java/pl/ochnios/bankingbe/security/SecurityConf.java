d9cb01a2606808a9f0f05433f0c3e26f86a3a99c
Szymon Ochnio <szymonochnio15@gmail.com>
2024-01-23 21:00:56 +0100

    Minor improvements (security).

--- banking-be/src/main/java/pl/ochnios/bankingbe/security/SecurityConf.java
+++ banking-be/src/main/java/pl/ochnios/bankingbe/security/SecurityConf.java
@@ -15,6 +15,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.header.writers.XXssProtectionHeaderWriter;
 
 @Configuration
 @EnableWebSecurity
@@ -55,6 +56,13 @@ public class SecurityConf {
 //                        // TEMP necessary to make h2-console available
 //                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
 //                )
+                .headers(headers ->
+                        headers.xssProtection(
+                                xss -> xss.headerValue(XXssProtectionHeaderWriter.HeaderValue.ENABLED_MODE_BLOCK)
+                        ).contentSecurityPolicy(
+                                cps -> cps.policyDirectives("script-src 'self' https://localhost; style-src 'self'; form-action 'self';")
+                        )
+                )
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
